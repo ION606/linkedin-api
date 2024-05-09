2b94b5ccbcd91f5b30cee1f1276a360d61ef5d4f
ION606 <ion606@protonmail.com>
2024-05-09 08:16:22 -0700

    restructuring

--- classes/classes.js
+++
@@ -1,7 +0,0 @@
-import linkedInAPIClass from "../index.js";
-import { LinkedInProfile } from "./Profile.js";
-import { Company } from "./Company.js";
-import { GenericEntity, Group, ReactionTypeCount, SocialActivityCounts } from './misc.js'
-
-
-export { linkedInAPIClass, LinkedInProfile, Company, GenericEntity, Group, ReactionTypeCount, SocialActivityCounts };
